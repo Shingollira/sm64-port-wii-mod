273ecc4ac897843a1a92f2f8ce838fb613216e5d
Gamardz <gamardz2000@gmail.com>
2026-02-22 19:54:20 -0600

    Update controller_wii.c
    ups..

--- src/pc/controller/controller_wii.c
+++ src/pc/controller/controller_wii.c
@@ -20,8 +20,6 @@
 #include "../configfile.h"
 #include <ogcsys.h>
 
-#include <gccore.h>
-
 static int nunchuk_center_x = 0;
 static int nunchuk_center_y = 0;
 static bool nunchuk_calibrated = false;
@@ -45,26 +43,6 @@ static void controller_wii_read(OSContPad *pad)
     PAD_ScanPads();
 u16 gcHeld = PAD_ButtonsHeld(0);
 
-// Si hay GC conectado (Dolphin siempre lo detecta si está activo)
-if (gcHeld)
-{
-    if (gcHeld & PAD_BUTTON_A) pad->button |= A_BUTTON;
-    if (gcHeld & PAD_BUTTON_B) pad->button |= B_BUTTON;
-    if (gcHeld & PAD_BUTTON_START) pad->button |= START_BUTTON;
-    if (gcHeld & PAD_BUTTON_Z) pad->button |= Z_TRIG;
-    if (gcHeld & PAD_BUTTON_R) pad->button |= R_TRIG;
-    if (gcHeld & PAD_BUTTON_L) pad->button |= L_TRIG;
-
-    if (gcHeld & PAD_BUTTON_UP) pad->button |= U_CBUTTONS;
-    if (gcHeld & PAD_BUTTON_DOWN) pad->button |= D_CBUTTONS;
-    if (gcHeld & PAD_BUTTON_LEFT) pad->button |= L_CBUTTONS;
-    if (gcHeld & PAD_BUTTON_RIGHT) pad->button |= R_CBUTTONS;
-
-    pad->stick_x = PAD_StickX(0);
-    pad->stick_y = PAD_StickY(0);
-
-    return; // prioridad al GC
-}
     WPAD_ScanPads();
 
     uint32_t held = WPAD_ButtonsHeld(0);
